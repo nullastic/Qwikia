dacfd21d8de1dc0a9ce79af63c435bb6be3606fa
Chelsea Tanchip <chelseanicolene1030@gmail.com>
2017-09-24 16:52:38 -0400

    Update question.js

--- question.js
+++ question.js
@@ -31,11 +31,68 @@ function getQuestionFromText(text, title) {
             var length = key.length;
             console.log(length);
             var newText = text;
-            var blank='';
+            var blank='________';
+            
+            //edits: chelsea
+            /**
             for(var i = 0; i < length; i++) {
                 blank+='_';
             }
+            */
+            
             newText = text.substr(0,index) + blank + text.substr(index+length);
+            trueKey = text.substr(0,index) + key + text.substr(index+length);
+            reformatQuestion(newText, length);
+            filterQuestion(trueKey, key);
+            
+            function reformatQuestion(text, length) {
+                //reformat question
+                var blank = "_";
+                
+                // WHO question
+                var sub1 = "is";
+                var sub2 = "was";
+                if (text.indexOf(sub1) !== -1 || text.indexOf(sub2) !== -1) {
+                    if (text.substr(text.indexOf(sub1)-2) == blank) {
+                        text = "Who is " + text.substr((text.indexOf(sub1)+3), length-1) + "?"
+                    }
+                    else if (text.substr(text.indexOf(sub2)-2) == blank) {
+                        text = "Who was " + text.substr((text.indexOf(sub1)+3), length-1) + "?"
+                    }
+                }
+                
+                // WHAT question
+                // how is it different from who?
+                
+                // WHERE question
+                
+                // WHY question
+                
+            }
+            
+            function filterQuestion(text, key) {
+                /**
+                check:
+                - if key is embedded in text
+                - if blank is a lexical stem of key
+                */
+                blank = "________";
+                if (text.indexOf(key) != -1) {
+                    if (text.substr(0, text.indexOf(key)).indexOf(key) != -1) {
+                        text = text.substr(0,text.indexOf(key)) + blank + 
+                            text.substr((text.indexOf(blank)+(blank.length+1)), text.length);
+                    }
+                    else if (text.substr((text.indexOf(key)+(key.length+1)), text.length).indexOf(key) != -1) {
+                        text = text.substr(0, text.indexOf(key)) + blank + 
+                            text.substr((text.indexOf(key) + key.length), text.length)
+                    }
+                }
+                
+            }
+            
+            // edits end here
+            
+            //
             request({
                   url: 'https://graph.facebook.com/v2.10/me/messages',
                   qs: {access_token: 'EAARiEsAuvXEBAHvp6kDS4bAcyIrkudgRZCieT78BWO7ZAsbfAzIdkjMe7EJlv731DezS6Ic5crJs2OOTZCIVXVf3GijGjnwzNRkcZAwJHJaFPfdERSsp9dvZCuKUnCchIEZCjE9BOv58Pcc6EdrKV3wSK5lkKkDLhqGFjwjUua0gZDZD'},
